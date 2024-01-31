71075d8ff78a77b2d34b5d558e2ff81a186ffb4f
shubhampatel12344 <shubhampatel12344@gmail.com>
2024-02-01 02:58:40 +1030

    Content added

--- src/sections/Education.vue
+++ src/sections/Education.vue
@@ -13,26 +13,72 @@ import Box from './Box.vue';
             <Toggle 
                 title="Professional Year"
                 link-title="Performance Education, Adelaide"
-                link="https://google.com"
+                link="https://www.performance.edu.au/story"
                 date="Feb 2023 - Oct 2023"
             >
-            Key Subjects: Australian Business Culture, Environment, and Workplace Skills, Professional Business Communication
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Key Courses: </td>
+            <td> Australian Business Culture, Environment, and Workplace Skills, Professional Business Communication. </td>
+            </tr>
+            </table>    
+
             </Toggle>
 
             <Toggle 
                 title="Bachelor of Information Technology (Networking and Cybersecurity)"
                 link-title="UniSA, Adelaide"
-                link="https://google.com"
+                link="https://www.unisa.edu.au/about-unisa/"
                 date="Feb 2019 - Dec 2022"
             >
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Key Courses: </td>
+            <td> ICT Capstone Project, Cloud, Virtualization and Storage, Digital Forensics, Web Development, Network Architecture, CCNP Enterprise Service, Agile Development and Governance, IT Project Management. </td>
+            </tr>
+            </table> 
+
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Key Electives: </td>
+            <td>  World History Trends and Transformations, Business Resilience, Security Governance. </td>
+            </tr>
+            </table> 
+
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Achievements: </td>
+            <td> 
+             <ul class="list-disc list-inside">
+              <li> Distinction average achieved overall. </li>
+              <li> International Merit Scholarship. </li>
+             </ul>
+            </td>
+            </tr>
+            </table> 
+
             </Toggle>
 
             <Toggle 
                 title="High School"
                 link-title="H.B.Kapadia New High School, Ahmedabad"
-                link="https://google.com"
+                link="https://en.wikipedia.org/wiki/H._B._Kapadia_New_High_School"
                 date="May 2017 - Mar 2018"
             >
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Achievements: </td>
+            <td> High Distinction achieved. </td>
+            </tr>
+            </table> 
+
             </Toggle>
         </div>
 
@@ -43,18 +89,68 @@ import Box from './Box.vue';
 
             <Toggle 
                 title="Data Security"
-                link-title="Moi Sante, Adelaide"
-                link="https://google.com"
+                link-title="Moi Sante"
                 date="Aug 2022 - Dec 2022"
             >
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Objective: </td>
+            <td> This project aimed to establish Moi Sante’s brand, allowing the supplier to concentrate on providing services rather than wasting time on administrative tasks and to provide real-time. </td>
+            </tr>
+            </table> 
+
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Skills: </td>
+            <td> Data Security, Integrity and Ethics, Security Auditing and Compliance, Firewall Management, Cybersecurity Analysis. </td>
+            </tr>
+            </table> 
+            
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Duties: </td>
+            <td> To accomplish this, strategies such as giving the users the ability to rate, refer and give feedback on the application were put in place to encourage user interaction and therefore improve their brand. Moi Sante’s requirements and problems were addressed using appropriate technical security methods and the consideration of business security. A proposed network included firewalls and attack mitigations while the company and staff were provided with training and security awareness. </td>
+            </tr>
+            </table> 
+
             </Toggle>
 
             <Toggle 
                 title="Course Evaluation"
-                link-title="Palo Alto, Adelaide"
-                link="https://google.com"
+                link-title="Palo Alto"
                 date="Jul 2021 - Dec 2021"
             >
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Objective: </td>
+            <td> Collaborated in a team of 4 to complete the project successfully. Compare the benefits of using Palo Alto Networking Academy with the existing CISCO Networking Academy. </td>
+            </tr>
+            </table> 
+
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Skills: </td>
+            <td> Continuous Learning, Attention to Detail, Leadership, Time Management, Teamwork. </td>
+            </tr>
+            </table> 
+            
+            <br />
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Duties: </td>
+            <td> I Investigated and completed one stream of the Palo Alto enterprise security certification through the Palo Alto Academy. Evaluated resources, labs, support, quizzes, and exams available to students and lecturers. Presented findings through a comprehensive presentation and detailed report. Explored content, supporting resources, support systems, costs, ease of implementation, and benefits to the University. </td>
+            </tr>
+            </table> 
+
             </Toggle>
         </div>
     </Box>
--- src/sections/Experience.vue
+++ src/sections/Experience.vue
@@ -13,19 +13,67 @@ import Box from './Box.vue';
             <Toggle 
                 title="Web Developer"
                 link-title="eBottli Pty. Ltd., Adelaide"
-                link="https://google.com"
+                link="https://ebottli.com/"
                 date="Oct 2023 - Present"
             >
-                Example Content
-                
+            
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Key Responsibilities: </td>
+            <td> 
+            </td>
+            </tr>
+            </table>   
+            <br/>
+            <ul class="list-disc list-outside">
+              <li> <b> Mobile Interface Development: </b> Led the development of the mobile interface on Android, enhancing client accessibility and engagement. Employed Android development tools and technologies to create a client-friendly and responsive application. </li>
+              <br/>
+              <li> <b> Web Interface Integration: </b> Successfully integrated the web interface with AWS and Angular, fostering a cohesive client experience. Leveraged AWS services for scalability and Angular for dynamic front-end design. </li>
+              <br/>
+              <li> <b> Database Integration: </b> Worked closely with Firebase database, incorporating new features and functionalities for websites, enriching client interactions and data-driven decision-making.</li>
+              <br/>
+              <li> <b> Bug Fixing and Optimization: </b> Proactively identified and resolved existing bugs in web applications, optimizing website performance and ensuring a smooth client experience.</li>
+              <br/>
+              <li> <b> Usability Audits and Efficiency Enhancement: </b> Regularly audited the website for usability, promptly addressing issues, and enhancing efficiency. Ensured that our web applications evolved to meet changing client needs.</li>
+             </ul>              
             </Toggle>
 
             <Toggle 
                 title="Customer Service Representative"
                 link-title="TSA Group, Adelaide"
-                link="https://google.com"
+                link="https://tsagroup.com.au/about/"
                 date="Feb 2023 - Oct 2023"
             >
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Key Responsibilities: </td>
+            <td> 
+            </td>
+            </tr>
+            </table>   
+            <br/>
+            <ul class="list-disc list-outside">
+            <li> Efficiently provided customer service via calls, addressing diverse inquiries and concerns for clients.</li>
+            <li> Acquired proficiency in resolving complaints, addressing objections, and scheduling appointments.</li>
+            <li> Assisted Priority Assist Customers, ensuring uninterrupted service for vulnerable individuals and delivering exceptional support across Telstra's offerings.</li>
+            <li> Managed service orders, upgrades, cancellations, and account changes accurately and efficiently, while providing technical assistance for device setup, troubleshooting, and optimization.</li>
+            </ul>
+
+            <br/>
+
+            <table class="w-full">
+            <tr>
+            <td class="w-1/4 align-top"> Achievements: </td>
+            <td> 
+             <ul class=" list-inside">
+              <li> <b> Brightest Team Member Jun 2023 </b> </li>
+              Brightest team member in the academy and stood by this in the production space too. Wonderful growth especially because learnt process on own and made good use of resources provided.
+             </ul>
+            </td>
+            </tr>
+            </table>
+
             </Toggle>
         </div>
 
@@ -37,17 +85,17 @@ import Box from './Box.vue';
             <Toggle 
                 title="Animal Welfare"
                 link-title="Farishtey, Ahmedabad"
-                link="https://google.com"
                 date="Mar 2022 - Jul 2022"
             >
+            Participated in fundraising activities and events to support the NGO's financial needs and raise awareness of animal welfare issues.
             </Toggle>
 
             <Toggle 
                 title="UniMentor"
                 link-title="UniSA, Adelaide"
-                link="https://google.com"
                 date="Jun 2020 - Dec 2020"
             >
+            As a UniSA representative and influential student offered a friendly face while providing ongoing support to new students, helping to make the transition to university easier.
             </Toggle>
         </div>
     </Box>
